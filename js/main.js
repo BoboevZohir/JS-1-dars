cad7b63f6e28bf513caa618b6a2bd44c50eb96d7
Boboev Zohir <zohirbabaev151@gmail.com>
2025-02-13 10:47:45 +0500

    JS 1-dars
    Zeromax It-center 40 guruh

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// JSga kirish
+
+//console.log("Men faqat konsole oynasida korinaman!");
+//alert("Saytimisga xush kelibsiz");
+//document.write("Men bodyda ko'rindim")
+
+// JS o'zgaruvchilari
+
+//let x = 5;
+//let y = 7;
+//let z = 10;
+//let x = 15; y= 7; z = 10;
+//document.write(x+y+z)
+
+// JSda ma'lumotlar turlari
+//let son = 45;                   //number
+//let matn = "Javascript";
+//document.write(typeof matn)     //string
+
+// JSda matematika
+let x = "61";
+
+//x+=61;   x=5+61;   qoshish
+//x-=3;    x+5-3;    ayirish
+//x*=23;   x=5*23;   ko'paytirish
+//x/=2.5;  x=5/2.5;  bolish
+//let qoldiq = x%4;  qoldiq chiqarish
+//let daraja = x**5  darajaga ko'tarish   // 5*5*5*5*5
+
+//++x;
+//x++;
+//--x;
+//x--;
+//document.write(x + 5)
+
+document.write()
